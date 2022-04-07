479e969ce295b9e38d44c7f0421e862cd61d0063
devtsan <102000082+devtsan@users.noreply.github.com>
2022-04-08 03:26:31 +0630

    Update menuoc.js

--- core/menuoc.js
+++ core/menuoc.js
@@ -1,5 +1,5 @@
 function menuoc() {
 	document.querySelector('#menu').classList.toggle('w3-hide-small');
-	document.querySelector('#mo').classList.toggle('w3-hide-small');
-	document.querySelector('#mc').classList.toggle('w3-hide-small');
+	document.querySelector('#mo').classList.toggle('w3-hide');
+	document.querySelector('#mc').classList.toggle('w3-hide');
 };
