910484515901e8eaa1729c46a2429c1dc0e3046d
devtsan <102000082+devtsan@users.noreply.github.com>
2022-04-07 13:09:59 +0630

    Create menuoc.js

---
+++ core/menuoc.js
@@ -0,0 +1,5 @@
+function menuoc() {
+	document.querySelector('#menu').classList.toggle('w3-hide-small');
+	document.querySelector('#mo').classList.toggle('w3-hide-small');
+	document.querySelector('#mc').classList.toggle('w3-hide-small');
+};
